84974cf4ed0b78d870e94cb520056a3a31f90075
Rooesh Reddy <roopesh1048@gmail.com>
2020-06-14 16:01:26 +0530

    first commit

---
+++ _header.php
@@ -0,0 +1,5 @@
+        <div class="header">
+        	<center>
+            <h1> Doctor Appointment Scheduling</h1>
+        </center>
+        </div>
---
+++ _navigation.php
@@ -0,0 +1,9 @@
+            <div class="space">
+            	<center>
+                <a href="index.php">Public</a>
+                |
+                <a href="doctor.php">Doctor</a>
+                |
+                <a href="manager.php">Manager</a>
+            </center>
+            </div>
